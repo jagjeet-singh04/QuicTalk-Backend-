19dd22ed09e1dad1f294efc0bc0b2ff9c5d6a5c8
Jagjeet <jagjeetsingh0424@gmail.com>
2025-06-20 06:16:51 +0530

    route fix

--- src/routes/message.route.js
+++ src/routes/message.route.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import { 
+  getMessages, 
+  getUsersForSidebar, 
+  sendMessage 
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
-router.get("/:id", protectRoute, getMessages);
-
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/:id", protectRoute, getMessages); // Correct - has parameter name
+router.post("/send/:id", protectRoute, sendMessage); // Correct - has parameter name
 
 export default router;
